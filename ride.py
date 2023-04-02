80a2c4e87aa085c40c7a9ded22b4c381148b9e8d
ananya0809 <ananya.199303010@muj.manipal.edu>
2023-04-02 01:36:25 -0400

    code to construct class for Ride service

---
+++ ride.py
@@ -0,0 +1,11 @@
+class Ride:
+    rideNumber: int
+    rideCost: int
+    tripDuration: int
+
+    def __init__(self, rn, rc, td):
+        self.rideNumber = rn
+        self.rideCost = rc
+        self.tripDuration = td
+        
+
