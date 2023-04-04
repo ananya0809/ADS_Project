afb07935fe82e1660a857cd3bd4399b26fb37195
ananya0809 <ananya.199303010@muj.manipal.edu>
2023-04-04 01:03:46 -0400

    code to put and get key value pairs

--- rbt_minheap.py
+++ rbt_minheap.py
@@ -4,10 +4,20 @@ class DictPair(dict):
     minhpIndex: int
 
     def __init__(self):
-        self = dict()
+        self.dict = dict()
     
     def put(self, key, rbtInd, minhpInd):
-        self[key] = {rbtInd, minhpInd}
+        self.dict[key] = (rbtInd, minhpInd)
+    
+    def get(self, key):
+        try:
+            return self.dict[key]
+        except:
+            self.put(key, -1, -1)
+            return self.get(key)
+
+    def printAll(self):
+        print (self.dict)
     
 
 
