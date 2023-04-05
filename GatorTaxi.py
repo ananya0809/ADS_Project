c89b63cc635aca39795c6a781883d78e8c2952b4
ananya0809 <ananya.199303010@muj.manipal.edu>
2023-04-05 12:22:18 -0400

    add code to use File Handling

--- GatorTaxi.py
+++ GatorTaxi.py
@@ -3,32 +3,17 @@ from rbtree import RedBlackTree
 from minheap import MinHeap
 from ride import Ride
 from rbt_minheap import DictPair
-
-# init
-rbt = RedBlackTree()
-minhp = MinHeap(2000)
+from input_output_handler import FileHandler
+from input_output_handler import OperationType
 
 # insert
-rides = [
-    Ride(25,98,46),
-    Ride(42,17,89),
-    Ride(9,76,31),
-    Ride(53,97,22),
-    Ride(68,41,51)
-    # Ride(27,17,88)
-]
 
-for ride in rides:
-    rbt.insert(ride)
-    minhp.insert(ride)
 
-# Dictionary for Pointers between MinHeap and Red Black Tree
-dictionary = DictPair()
-def Pointers(allrides):
-        for ride in range(0, len(allrides)):
-            dictionary.key = ride
-            dictionary.value = {rbt.search()}
-            dictionary.put(dictionary.key, dictionary.value)
+# def Pointers(allrides):
+#         for ride in range(0, len(allrides)):
+#             dictionary.key = ride
+#             dictionary.value = {rbt.search()}
+#             dictionary.put(dictionary.key, dictionary.value)
 
 
 
@@ -73,34 +58,7 @@ def GetNextRide():
     for remainingRides in removedRides:
         minhp.insert(remainingRides)
 
-    return minTrip
-
-    # print(minTrip.to_str())
-    # print(aRide.to_str())
-
-    # while minhp.size != 0:
-    #     removeRide = minhp.remove()
-    #     removedRides.append(removeRide)
-    # for aRide in removedRides:
-    #     print(aRide)
-    #     if aRide.rideCost == (aRide+1).rideCost:
-    #         if removedRides[index].tripDuration < removedRides[index+1].tripDuration:
-    #             return aRide
-    #         else:
-    #             return (index+1)
-    #     else:
-    #         return aRide
-
-
-    # tempRide = minhp.remove()
-
-    # if removeRide.rideCost == tempRide.rideCost:
-    #     if removeRide.tripDuration < tempRide.tripDuration:
-    #         return removeRide
-    #     else:
-    #         return tempRide
-    # else:
-    #     return removeRide
+    return minTrip    
 
 # Operation 5
 def CancelRide(rideNumber):
@@ -109,7 +67,19 @@ def CancelRide(rideNumber):
         rbt.delete(toDelete)
     
 
+# Dictionary for Pointers between MinHeap and Red Black Tree
+dictionary = DictPair()
+
+# init
+rbt = RedBlackTree(dictionary)
+minhp = MinHeap(100, dictionary)
+
+# File Handling between Input and Output file
+operations = FileHandler().parseFile()
+
+print(operations)
+
 # print(print_ridenum(9).ride.to_str())
 # print_ride1_ride2(4, 31)
-print(GetNextRide().to_str())
+# print(GetNextRide().to_str())
 
